bc280891842c78621289ce9dbce8c28fed8af82c
LupaSeal <110409402+LupaSeal@users.noreply.github.com>
2023-05-23 10:43:48 +0300

    Create ATM.java

---
+++ ATM.java
@@ -0,0 +1,12 @@
+/* Main Class
+@Author Alin A.*/
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+
+		optionMenu.getLogin();
+	}
+}
